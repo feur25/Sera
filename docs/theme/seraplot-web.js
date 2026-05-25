0679102d1b8763e8e72ccf7c4ad604ba6a398931
feur25 <feur09@gmail.com>
2026-05-25 18:07:28 +0200

    fix(wasm): expose window.SeraplotWASM from seraplot-web.js

--- docs/theme/seraplot-web.js
+++ docs/theme/seraplot-web.js
@@ -2182,3 +2182,7 @@ let wasm_bindgen = (function(exports) {
 
     return Object.assign(__wbg_init, { initSync }, exports);
 })({ __proto__: null });
+window.SeraplotWASM = wasm_bindgen;
+window.SeraplotWASM.__init = function(path) {
+    return wasm_bindgen(path).then(function() { wasm_bindgen.__ready = true; });
+};
