6426ddb5f4ec201872cee2d47750ffdac37a7563
feur25 <feur09@gmail.com>
2026-08-25 16:50:05 +0200

    refactor(default): submit line/scatter/bar chart types via inventory
    register_default_types() now just calls register_group_from_inventory,
    same shape as register_default_3d_types(). Adds the well-formed/
    matches-inventory test pair the 3D sibling already has.

--- services/plot/default/chart.rs
+++ services/plot/default/chart.rs
@@ -2,60 +2,39 @@ use crate::plot::controller::chart_controller::*;
 use std::ffi::CStr;
 use std::os::raw::c_char;
 
-const DEFAULT_RENDERERS: &[(u8, &str, ChartRenderer)] = &[
-    (0, "line", super::render_lines as ChartRenderer),
-    (1, "scatter", super::render_points as ChartRenderer),
-    (2, "bar", super::render_bars as ChartRenderer),
-];
-
-const DEFAULT_SVG_RENDERERS: &[(
-    u8,
-    crate::plot::controller::chart_controller::SvgChartRenderer,
-)] = &[
-    (
-        0,
-        super::line::render_svg_lines
-            as crate::plot::controller::chart_controller::SvgChartRenderer,
-    ),
-    (
-        1,
-        super::scatter::render_svg_scatter
-            as crate::plot::controller::chart_controller::SvgChartRenderer,
-    ),
-    (
-        2,
-        super::bar::render_svg_bars as crate::plot::controller::chart_controller::SvgChartRenderer,
-    ),
-];
-
-const DEFAULT_COLORS: &[(u8, u32)] = &[(0, 0x50c878), (1, 0xf39c12), (2, 0x4a90e2)];
+inventory::submit! {
+    ChartTypeEntry {
+        group: "default",
+        id: 0,
+        name: "line",
+        renderer: super::render_lines as ChartRenderer,
+        svg_renderer: Some(super::line::render_svg_lines as SvgChartRenderer),
+        color: 0x50c878,
+    }
+}
+inventory::submit! {
+    ChartTypeEntry {
+        group: "default",
+        id: 1,
+        name: "scatter",
+        renderer: super::render_points as ChartRenderer,
+        svg_renderer: Some(super::scatter::render_svg_scatter as SvgChartRenderer),
+        color: 0xf39c12,
+    }
+}
+inventory::submit! {
+    ChartTypeEntry {
+        group: "default",
+        id: 2,
+        name: "bar",
+        renderer: super::render_bars as ChartRenderer,
+        svg_renderer: Some(super::bar::render_svg_bars as SvgChartRenderer),
+        color: 0x4a90e2,
+    }
+}
 
 pub fn register_default_types() {
-    let mut ids = Vec::new();
-    for (id, name, renderer) in DEFAULT_RENDERERS {
-        if let Err(e) = ChartTypeBuilder::new(*id)
-            .with_name(name)
-            .with_renderer(*renderer)
-            .build()
-        {
-            eprintln!("seraplot: failed to register default chart type '{name}' (id {id}): {e}");
-        }
-        ids.push(*id);
-    }
-
-    for (id, svg_renderer) in DEFAULT_SVG_RENDERERS {
-        with_registry_mut("register_default_types/svg", |reg| reg.register_svg(*id, *svg_renderer));
-    }
-
-    for (id, color) in DEFAULT_COLORS {
-        with_registry_mut("register_default_types/color", |reg| reg.register_color(*id, *color));
-    }
-
-    if let Ok(mut grp_reg) = get_group_registry().lock() {
-        grp_reg.register_group("default".to_string(), ids);
-    } else {
-        eprintln!("seraplot: failed to register 'default' chart group, group registry lock poisoned");
-    }
+    register_group_from_inventory("default");
 }
 
 #[no_mangle]
@@ -78,4 +57,19 @@ pub extern "C" fn sera_register_chart_type(id: u8, name: *const c_char, renderer
         .with_renderer(renderer)
         .build()
         .is_ok()
+}
+
+#[cfg(test)]
+mod inventory_tests {
+    use crate::plot::controller::chart_controller::test_support::*;
+
+    #[test]
+    fn default_group_is_well_formed() {
+        assert_group_well_formed("default");
+    }
+
+    #[test]
+    fn register_default_types_matches_inventory() {
+        assert_registered_group_matches_inventory("default", super::register_default_types);
+    }
 }
