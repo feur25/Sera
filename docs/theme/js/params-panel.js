a36025520d87bce1789963f2c66260c8ff09e7da
feur25 <feur09@gmail.com>
2026-08-30 00:23:15 +0200

    chore(wasm): deploy bundle 20260830a -- 193 region sets now (14.6MB)
    verified live against mdbook serve: build=20260830a, ready=true, 0 console
    errors.

--- docs/theme/js/params-panel.js
+++ docs/theme/js/params-panel.js
@@ -1,4 +1,4 @@
-window.SP_WASM_BUILD = window.SP_WASM_BUILD || "20260829r";
+window.SP_WASM_BUILD = window.SP_WASM_BUILD || "20260830a";
 (function () {
   var SP_SELF_SRC = (document.currentScript && document.currentScript.src) || "";
   var SP_WASM_BUILD = window.SP_WASM_BUILD;
