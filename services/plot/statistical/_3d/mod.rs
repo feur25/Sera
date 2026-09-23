a97287d663153561d41041f297677ef165e99d7e
feur25 <feur09@gmail.com>
2026-09-23 23:56:35 +0200

    chore(3d): drop the dead render_ridgeline3d_html re-export

--- services/plot/statistical/_3d/mod.rs
+++ services/plot/statistical/_3d/mod.rs
@@ -91,5 +91,4 @@ pub use mesh3d::{cone_mesh, cube_mesh, grid_to_mesh, render_mesh3d_html};
 pub use pie3d::render_pie3d_html;
 pub use plot_3d_types::register_statistical_3d_types;
 pub use radar3d::render_radar3d_html;
-pub use ridgeline3d::render_ridgeline3d_html;
 pub use stacked_bar3d::render_stacked_bar3d_html;
